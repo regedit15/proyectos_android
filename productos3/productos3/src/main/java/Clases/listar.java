8e0a9c7b1478834918eb71b8c8738490a614ac15
regedit15 <001regedit15@gmail.com>
2013-10-12 12:38:36 -0300

    se subio el proyecto al repositorio

---
+++ productos3/productos3/src/main/java/Clases/listar.java
@@ -0,0 +1,19 @@
+package Clases;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import prueba.productos3.R;
+
+/**
+ * Created by martincho on 01/10/13.
+ */
+public class listar extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.listar);
+    }
+}
