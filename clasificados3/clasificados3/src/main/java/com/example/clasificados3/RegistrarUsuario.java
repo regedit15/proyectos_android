79115f829855bde9e6c7abcaaa4cfe45da2594a9
regedit15 <001regedit15@gmail.com>
2013-11-09 13:44:39 -0300

    se creo la clase clasificados 3. Se hizo la facuada del login con el cambio de layout al reguistrar

---
+++ clasificados3/clasificados3/src/main/java/com/example/clasificados3/RegistrarUsuario.java
@@ -0,0 +1,18 @@
+package com.example.clasificados3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by martincho on 09/11/13.
+ */
+public class RegistrarUsuario extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrar_usuario);
+    }
+
+}
